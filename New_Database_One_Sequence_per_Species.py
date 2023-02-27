89f1bf61321c2294cd362cf424ef5fd65db9f494
Hal Canary <halcanary@gmail.com>
2023-02-27 11:03:02 -0500

    [BUGFIX] `get_best_sequence` and other changes.
    `New_Database_One_Sequence_per_Species`:
    
    *   [BUGFIX] fix `get_best_sequence`
    *   Mark code as MIT License
    *   use path_modifier
    *   let genus be empty, meaning do not filter by genus
    *   Document `main`
    *   Fail on no matching sequences; produce no output file
    *   Usage message on `-help` (or anything else)
    
    README:
    
    *   update copyright, license.
    *   install instructions.
    *   program documentation.

--- New_Database_One_Sequence_per_Species.py
+++ New_Database_One_Sequence_per_Species.py
@@ -1,20 +1,27 @@
 #! /usr/bin/env python
 
-# Copyright 2023 Hal W Canary III, Lindsay R Saunders PhD. ALL RIGHTS RESERVED.
+# Copyright 2023 Hal W Canary III, Lindsay R Saunders PhD.
+# Use of this program is governed by contents of the LICENSE file.
 
-import sys
+import collections
 import os
+import sys
 
 DEFAULT_GENUS = 'Glutamicibacter'
 DEFAULT_INPUT_FILE = os.path.expanduser('~/Desktop/SP23_NewGeneraDatabases/Glutamicibacter_spp.fasta')
-DEFAULT_OUTPUT_FILE = os.path.expanduser('~/Desktop/SP23_NewGeneraDatabases/Glutamicibacter_spp_16s.fasta')
+DEFAULT_PATH_MODIFIER = '_16s'
 
 VERBOSE = True
 
 ################################################################################
-# fasta format parsing
+# FASTA file format parsing functions
+# These may be moved to another package at a later time.
+################################################################################
 
 def parse_fasta_format(f):
+    '''
+    @param f file object open for reading in FASTA format.
+    '''
     description, sequence = None, None
     for line in f:
         if line.startswith('>'):
@@ -28,90 +35,118 @@ def parse_fasta_format(f):
         yield (description, sequence)
 
 def print_fasta_description(o, description, sequence):
+    '''
+    @param o file object open for writing.
+    @param description string describing the sequence.
+    @param sequence string in FASTA Sequence Representation.
+    '''
     o.write('>%s\n' % description) # prepend '>' character.
     for i in range(0, len(sequence), 70):
-        o.write(sequence[i:i+70]) 
+        o.write(sequence[i:i+70])
         o.write('\n')
     o.write('\n')
-    
+
 ################################################################################
 
 def get_values_matching_genus(given_genus, inputf):
-    #all_values[species_name] = [ (accession_number, desc, sequence), .... ]
-    all_values = {}
-    count, match_count = 0, 0
+    '''
+    @param given_genus If not '', will filter the sequences by genus.
+    @param inputf file object open for reading in FASTA format.
+           Each description line is expected to be in the form:
+           >ACCESSION_NUMBER GENUS EPITHET EXTRA ...
+    @return dictionary keyed by species ("{GENUS}_{EPITHET}") with values
+            that are lists of tuples, where each tuple is
+            `(accession_number, changed_description, sequence)`.
+            The new decription is the form:
+            >GENUS_EPITHET ACCESSION_NUMBER EXTRA ...
+    '''
+    all_values = collections.defaultdict(list)
+    count = 0
     for description, value in parse_fasta_format(inputf):
         count += 1
-        accession, genus, epithet, extra = description.split(maxsplit=3)
-        species = genus + '_' + epithet
-        if extra is None or genus != given_genus:
+        try:
+            accession, genus, epithet, extra = description.split(maxsplit=3)
+        except ValueError as e:
+            sys.stderr.write('ERROR: Unable to parse description: %r\n' % description)
+            continue
+        if given_genus != '' and genus != given_genus:
             if VERBOSE:
                 sys.stderr.write('BAD MATCH:  %r\n' % description)
             continue
-        sys.stderr.write('good match: %r\n' % description)
+        if VERBOSE:
+            sys.stderr.write('good match: %r\n' % description)
+
+        species = genus + '_' + epithet
         new_description = ' '.join([species, accession, extra])
-        if species not in all_values:
-            all_values[species] = []
         all_values[species].append((accession, new_description, value))
-        match_count += 1
     if VERBOSE:
+        match_count = sum(len(v) for v in all_values.values())
         sys.stderr.write('\nMatched %d of %d.\n' % (match_count, count))
-    return all_values
+    return dict(all_values)
 
-
-def calculate_score(accession, description, sequence):
-    count = sum(1 for c in sequence if c in ['A', 'G', 'T', 'C'])
-    return (1 if ' type strain ' in description else 0,
-            count / len(sequence),
-            1 if accession.startswith('NR_') else 0,
-            len(sequence))
-
-
-def get_best_sequence(values):
-    # values = [ (accession_number, description, sequence), .... ]
-    best = (None, None)
-    if len(values) > 0:
-        best_score = calculate_score(*values[0])
-        for accession, description, sequence in values[1:]:
-            score = calculate_score(accession, description, sequence)
-            if score > best_score:
-                best_score = score
-                best = (description, sequence)
-    return best
-    
 
 def process(all_values, outputf):
+    '''
+    @param all_values dictionary in the form returned by `get_values_matching_genus`.
+    @param outputf file object open for writing.
+    '''
     if VERBOSE:
         sys.stderr.write('\n')
-    #all_values[species_name] = [ (accession_number, description, sequence), .... ]
-    #>species accesion_number extra stuff
     count = 0
+    # Sort output by sepcies for reproducability.
     for species, values in sorted(all_values.items()):
-        assert len(values) > 0
-        description, sequence = get_best_sequence(values)
-        if description is not None:
-            print_fasta_description(outputf, description, sequence)
+        best_description, best_sequence, best_score = None, None, (-1, -1, -1, -1)
+        for accession, description, sequence in values:
+            # score is a comparable 4-tuple of non-negative numbers.
+            score = (1 if ' type strain ' in description else 0,
+                    float(sum(1 for c in sequence if c in ['A', 'G', 'T', 'C'])) / len(sequence),
+                    1 if accession.startswith('NR_') else 0,
+                    len(sequence))
+            if score >= best_score:
+                best_description, best_sequence, best_score = description, sequence, score
+        if best_description and best_sequence:
+            print_fasta_description(outputf, best_description, best_sequence)
             count += 1
             if VERBOSE:
                 sys.stderr.write('Species %r, best of %d\n' %(species, len(values)))
     if VERBOSE:
         sys.stderr.write('\n%d of %d species processed.\n\n' % (count, len(all_values)))
 
-def main(genus, input_file, output_file):
+
+def main(given_genus, input_file, pathmod=DEFAULT_PATH_MODIFIER):
+    '''
+    @param given_genus If not '', will filter the sequences by genus.
+    @param input_file The path of the FASTA format input file.
+    @param pathmod If imput_file is 'foo/bar.baz.fasta' and pathmod is
+           '_filtered', then the output file will be
+           'foo/bar.baz_filtered.fasta'.
+    @return 0 on success, 1 othewrwise.
+    '''
+    if pathmod == '':
+        pathmod = DEFAULT_PATH_MODIFIER
+    pathroot, pathext = os.path.splitext(input_file)
+    output_file = pathroot + pathmod + pathext
     if VERBOSE:
-        sys.stderr.write('Genus  is %r\n' % genus)
+        sys.stderr.write('Genus  is %r\n' % given_genus)
         sys.stderr.write('Input  is %r\n' % input_file)
         sys.stderr.write('Output is %r\n\n' % output_file)
     with open(input_file) as f:
-        all_values = get_values_matching_genus(genus, f)
+        all_values = get_values_matching_genus(given_genus, f)
+    if len(all_values) == 0:
+        sys.stderr.write('no sequences match.\n')
+        return 1
     with open(output_file, 'w') as o:
         process(all_values, o)
     if VERBOSE:
         sys.stderr.write('wrote to %r\n' % (output_file))
-                
+    return 0
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 4:
-        main(*sys.argv[1:4])
+        sys.exit(main(*sys.argv[1:4]))
+    elif len(sys.argv) == 1:
+        sys.exit(main(DEFAULT_GENUS, DEFAULT_INPUT_FILE, DEFAULT_PATH_MODIFIER))
     else:
-        main(DEFAULT_GENUS, DEFAULT_INPUT_FILE, DEFAULT_OUTPUT_FILE)
+        sys.stderr.write('\nUsage:\n %s GENUS INPUT_FILE_PATH PATH_MODIFIER_STRING\n\n' % sys.argv[0])
+        sys.exit(1)
--- README.md
+++ README.md
@@ -1,3 +1,89 @@
-Copyright 2023 Hal W Canary III
+# Bioscript
 
-ALL RIGHTS RESERVED.
+Biology Scripts
+
+*Copyright 2023 Hal W Canary III, Lindsay R Saunders PhD.*  
+*Use of this program is governed by contents of the LICENSE file.*
+
+* * *
+
+## Install
+
+1.  Install Python 2.7.x or 3.x (3.x preferrably). <https://www.python.org/downloads/>
+
+    Verify that the command
+
+    ```
+    python --version
+    ```
+
+    or
+
+    ```
+    python3 --version
+    ```
+
+    works.  Remember which one and use that when running the scripts.
+
+2.  Clone this repo:
+
+    ```
+    cd ~
+    git clone https://github.com/HalCanary/bioscript.git
+    ```
+
+    or, if you have Github permissions:
+
+    ```
+    cd ~
+    git clone git@github.com:HalCanary/bioscript.git
+    ```
+
+* * *
+
+## Running `New_Database_One_Sequence_per_Species.py`
+
+Suppose the input file is `~/Desktop/foobar.fasta` and the genus to be filterted is `Foobar`.
+
+Run:
+
+```
+python ~/bioscript/New_Database_One_Sequence_per_Species.py Foobar ~/Desktop/foobar.fasta _new
+```
+
+This should write to a file `~/Desktop/foobar_new.fasta`.
+
+What it does:
+
+*   Descriptions of the form
+
+    ```
+    >XX999999.9 Foobar baz strain Ind Int 3 16S ribosomal RNA gene, partial sequence
+    ```
+
+    will be renamed
+
+    ```
+    >Foobar_baz XX999999.9 strain Ind Int 3 16S ribosomal RNA gene, partial sequence
+    ```
+
+*   Given the above input description, the genus is assumed to be `Foobar`.
+    Only exact matches for the genus are used.
+
+*   Given the above input description, the species is assumed to be `Foobar_baz`.
+
+    Only the "best" sequence for each species is kept, according to the
+    following criteria:
+
+    1.  If one has the string "` type strain `" in its description and the
+        other does not, use the former, otherwise:
+
+    2.  If one has a higher ratio of `AGTC` characters in the sequence than the other,
+        use that one, but otherwise:
+
+    3.  If one's accession string starts with "`NR_`", and the other does not,
+        use that one, otherwise:
+
+    4.  Use the one with the longer sequence.
+
+* * *
