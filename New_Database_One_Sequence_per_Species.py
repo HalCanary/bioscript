89d4b63bca0528067000bb002b2b85fb4492933d
Hal Canary <halcanary@gmail.com>
2023-02-27 12:52:34 -0500

    refactor for unit tests, VERY_VERBOSE, TODO list

--- New_Database_One_Sequence_per_Species.py
+++ New_Database_One_Sequence_per_Species.py
@@ -12,6 +12,13 @@ DEFAULT_INPUT_FILE = os.path.expanduser('~/Desktop/SP23_NewGeneraDatabases/Gluta
 DEFAULT_PATH_MODIFIER = '_16s'
 
 VERBOSE = True
+VERY_VERBOSE = False
+
+# TODO (halcanary): remove DEFAULT_GENUS and DEFAULT_INPUT_FILE.
+# TODO (halcanary): can we get GENUS from INPUT_FILE?
+# TODO (halcanary): should user change VERBOSE, VERY_VERBOSE with environment
+#                   variable?
+# TODO (halcanary): write unit tests for all functions apart from `main()`
 
 ################################################################################
 # FASTA file format parsing functions
@@ -21,6 +28,7 @@ VERBOSE = True
 def parse_fasta_format(f):
     '''
     @param f file object open for reading in FASTA format.
+    @yield tuples of form (description, sequence)
     '''
     description, sequence = None, None
     for line in f:
@@ -48,70 +56,62 @@ def print_fasta_description(o, description, sequence):
 
 ################################################################################
 
-def get_values_matching_genus(given_genus, inputf):
+def truncate(s, l):
+    return s if len(s) <= l else s[:l-3] + '...'
+
+def get_values_matching_genus(given_genus, fasta_source, verbose):
     '''
     @param given_genus If not '', will filter the sequences by genus.
-    @param inputf file object open for reading in FASTA format.
+    @param fasta_source FASTA source, a list (or generator) of tuples of
+           form `(description, value)`.
            Each description line is expected to be in the form:
            >ACCESSION_NUMBER GENUS EPITHET EXTRA ...
-    @return dictionary keyed by species ("{GENUS}_{EPITHET}") with values
+    @return tuple of (count, dictionary) where count is the number of read
+            sequences from the source and the dictionary is
+            keyed by species ("{GENUS}_{EPITHET}") with values
             that are lists of tuples, where each tuple is
             `(accession_number, changed_description, sequence)`.
             The new decription is the form:
             >GENUS_EPITHET ACCESSION_NUMBER EXTRA ...
     '''
-    all_values = collections.defaultdict(list)
-    count = 0
-    for description, value in parse_fasta_format(inputf):
+    count, all_values = 0, collections.defaultdict(list)
+    for (description, value) in fasta_source:
         count += 1
         try:
             accession, genus, epithet, extra = description.split(maxsplit=3)
-        except ValueError as e:
-            sys.stderr.write('ERROR: Unable to parse description: %r\n' % description)
-            continue
+        except ValueError:
+            try:
+                (accession, genus, epithet), extra = description.split(maxsplit=2), ''
+            except ValueError:
+                sys.stderr.write('ERROR: Unable to parse description: %r\n' % description)
+                continue
         if given_genus != '' and genus != given_genus:
-            if VERBOSE:
-                sys.stderr.write('BAD MATCH:  %r\n' % description)
+            if verbose:
+                sys.stderr.write('BAD MATCH:  %s\n' % truncate(description, 68))
             continue
-        if VERBOSE:
-            sys.stderr.write('good match: %r\n' % description)
+        if verbose:
+            sys.stderr.write('good match: %s\n' % truncate(description, 68))
 
-        species = genus + '_' + epithet
+        species = '%s_%s' % (genus, epithet)
         new_description = ' '.join([species, accession, extra])
         all_values[species].append((accession, new_description, value))
-    if VERBOSE:
-        match_count = sum(len(v) for v in all_values.values())
-        sys.stderr.write('\nMatched %d of %d.\n' % (match_count, count))
-    return dict(all_values)
+    return count, dict(all_values)
 
 
-def process(all_values, outputf):
-    '''
-    @param all_values dictionary in the form returned by `get_values_matching_genus`.
-    @param outputf file object open for writing.
-    '''
-    if VERBOSE:
-        sys.stderr.write('\n')
-    count = 0
-    # Sort output by sepcies for reproducability.
-    for species, values in sorted(all_values.items()):
-        best_description, best_sequence, best_score = None, None, (-1, -1, -1, -1)
-        for accession, description, sequence in values:
-            # score is a comparable 4-tuple of non-negative numbers.
-            score = (1 if ' type strain ' in description else 0,
-                    float(sum(1 for c in sequence if c in ['A', 'G', 'T', 'C'])) / len(sequence),
-                    1 if accession.startswith('NR_') else 0,
-                    len(sequence))
-            if score >= best_score:
-                best_description, best_sequence, best_score = description, sequence, score
-        if best_description and best_sequence:
-            print_fasta_description(outputf, best_description, best_sequence)
-            count += 1
-            if VERBOSE:
-                sys.stderr.write('Species %r, best of %d\n' %(species, len(values)))
-    if VERBOSE:
-        sys.stderr.write('\n%d of %d species processed.\n\n' % (count, len(all_values)))
+def process_values(values):
+    best_description, best_sequence, best_score = None, None, (-1, -1, -1, -1)
+    for accession, description, sequence in values:
+        # score is a comparable 4-tuple of non-negative numbers.
+        score = (1 if ' type strain ' in description else 0,
+                float(sum(1 for c in sequence if c in ['A', 'G', 'T', 'C'])) / len(sequence),
+                1 if accession.startswith('NR_') else 0,
+                len(sequence))
+        if score >= best_score:
+            best_description, best_sequence, best_score = description, sequence, score
+    return (best_description, best_sequence)
 
+
+################################################################################
 
 def main(given_genus, input_file, pathmod=DEFAULT_PATH_MODIFIER):
     '''
@@ -130,15 +130,29 @@ def main(given_genus, input_file, pathmod=DEFAULT_PATH_MODIFIER):
         sys.stderr.write('Genus  is %r\n' % given_genus)
         sys.stderr.write('Input  is %r\n' % input_file)
         sys.stderr.write('Output is %r\n\n' % output_file)
+
     with open(input_file) as f:
-        all_values = get_values_matching_genus(given_genus, f)
+        count, all_values = get_values_matching_genus(given_genus, parse_fasta_format(f), VERY_VERBOSE)
+    if VERBOSE:
+        sys.stderr.write('Read %d sequences.\n' % count)
+        sys.stderr.write('Matched %d sequences.\n\n' % sum(len(v) for v in all_values.values()))
     if len(all_values) == 0:
         sys.stderr.write('no sequences match.\n')
         return 1
+
     with open(output_file, 'w') as o:
-        process(all_values, o)
+        # Sort output by sepcies for reproducability.
+        for species, values in sorted(all_values.items()):
+            best_description, best_sequence = process_values(values)
+            if not best_description or not best_sequence:
+                sys.stderr.write('ERROR: unexpected problem with species %r.' % species)
+                continue
+            print_fasta_description(o, best_description, best_sequence)
+            if VERY_VERBOSE:
+                sys.stderr.write('Best of %3d for species %r\n' % (len(all_values[species]), species))
     if VERBOSE:
-        sys.stderr.write('wrote to %r\n' % (output_file))
+        sys.stderr.write('%d species processed.\n' % len(all_values))
+        sys.stderr.write('Wrote to file %r\n\n' % (output_file))
     return 0
 
 
