18af69e0c110e28751dd7ac0b662dcc9adf7f063
Hal Canary <halcanary@gmail.com>
2023-02-28 09:47:41 -0500

    logging instead of stderr.write

--- New_Database_One_Sequence_per_Species.py
+++ New_Database_One_Sequence_per_Species.py
@@ -4,21 +4,26 @@
 # Use of this program is governed by contents of the LICENSE file.
 
 import collections
+import logging
 import os
 import sys
 
 DEFAULT_GENUS = 'Glutamicibacter'
 DEFAULT_INPUT_FILE = os.path.expanduser('~/Desktop/SP23_NewGeneraDatabases/Glutamicibacter_spp.fasta')
 DEFAULT_PATH_MODIFIER = '_16s'
+DEFAULT_LOG_LEVEL = 'INFO'
 
-VERBOSE = True
-VERY_VERBOSE = False
+# NOTE: log level can be overridden with the
+# BIOSCRIPT_LOG_LEVEL environment variable:
+#   'DEBUG' for diagnosing bugs.
+#   'INFO' for succinct information.
+#   'WARNING' for quieter behavior.
 
 # TODO (halcanary): remove DEFAULT_GENUS and DEFAULT_INPUT_FILE.
-# TODO (halcanary): can we get GENUS from INPUT_FILE?
-# TODO (halcanary): should user change VERBOSE, VERY_VERBOSE with environment
-#                   variable?
+# TODO (halcanary): can we get GENUS from INPUT_FILE name?
 # TODO (halcanary): write unit tests for all functions apart from `main()`
+
+################################################################################
 
 ################################################################################
 # FASTA file format parsing functions
@@ -59,7 +64,7 @@ def print_fasta_description(o, description, sequence):
 def truncate(s, l):
     return s if len(s) <= l else s[:l-3] + '...'
 
-def get_values_matching_genus(given_genus, fasta_source, verbose):
+def get_values_matching_genus(given_genus, fasta_source):
     '''
     @param given_genus If not '', will filter the sequences by genus.
     @param fasta_source FASTA source, a list (or generator) of tuples of
@@ -83,14 +88,14 @@ def get_values_matching_genus(given_genus, fasta_source, verbose):
             try:
                 (accession, genus, epithet), extra = description.split(maxsplit=2), ''
             except ValueError:
-                sys.stderr.write('ERROR: Unable to parse description: %r\n' % description)
+                logging.error.write('Unable to parse description: %r', description)
                 continue
         if given_genus != '' and genus != given_genus:
-            if verbose:
-                sys.stderr.write('BAD MATCH:  %s\n' % truncate(description, 68))
+            if logging.getLogger().isEnabledFor(logging.DEBUG):
+                logging.debug('BAD MATCH:  %s', truncate(description, 68))
             continue
-        if verbose:
-            sys.stderr.write('good match: %s\n' % truncate(description, 68))
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            logging.debug('good match: %s', truncate(description, 68))
 
         species = '%s_%s' % (genus, epithet)
         new_description = ' '.join([species, accession, extra])
@@ -113,7 +118,7 @@ def process_values(values):
 
 ################################################################################
 
-def main(given_genus, input_file, pathmod=DEFAULT_PATH_MODIFIER):
+def main(given_genus, input_file, pathmod):
     '''
     @param given_genus If not '', will filter the sequences by genus.
     @param input_file The path of the FASTA format input file.
@@ -122,22 +127,20 @@ def main(given_genus, input_file, pathmod=DEFAULT_PATH_MODIFIER):
            'foo/bar.baz_filtered.fasta'.
     @return 0 on success, 1 othewrwise.
     '''
-    if pathmod == '':
-        pathmod = DEFAULT_PATH_MODIFIER
     pathroot, pathext = os.path.splitext(input_file)
     output_file = pathroot + pathmod + pathext
-    if VERBOSE:
-        sys.stderr.write('Genus  is %r\n' % given_genus)
-        sys.stderr.write('Input  is %r\n' % input_file)
-        sys.stderr.write('Output is %r\n\n' % output_file)
+
+    logging.info('Genus  is %r', given_genus)
+    logging.info('Input  is %r', input_file)
+    logging.info('Output is %r', output_file)
 
     with open(input_file) as f:
-        count, all_values = get_values_matching_genus(given_genus, parse_fasta_format(f), VERY_VERBOSE)
-    if VERBOSE:
-        sys.stderr.write('Read %d sequences.\n' % count)
-        sys.stderr.write('Matched %d sequences.\n\n' % sum(len(v) for v in all_values.values()))
+        count, all_values = get_values_matching_genus(given_genus, parse_fasta_format(f))
+    if logging.getLogger().isEnabledFor(logging.INFO):
+        logging.info('Read %d sequences.', count)
+        logging.info('Matched %d sequences.', sum(len(v) for v in all_values.values()))
     if len(all_values) == 0:
-        sys.stderr.write('no sequences match.\n')
+        logging.error('No sequences match.')
         return 1
 
     with open(output_file, 'w') as o:
@@ -145,20 +148,24 @@ def main(given_genus, input_file, pathmod=DEFAULT_PATH_MODIFIER):
         for species, values in sorted(all_values.items()):
             best_description, best_sequence = process_values(values)
             if not best_description or not best_sequence:
-                sys.stderr.write('ERROR: unexpected problem with species %r.' % species)
+                logging.error('Unexpected problem with species %r.', species)
                 continue
             print_fasta_description(o, best_description, best_sequence)
-            if VERY_VERBOSE:
-                sys.stderr.write('Best of %3d for species %r\n' % (len(all_values[species]), species))
-    if VERBOSE:
-        sys.stderr.write('%d species processed.\n' % len(all_values))
-        sys.stderr.write('Wrote to file %r\n\n' % (output_file))
+            if logging.getLogger().isEnabledFor(logging.DEBUG):
+                logging.debug('Best of %3d for species %r', len(all_values[species]), species)
+    logging.info('%d species processed.', len(all_values))
+    logging.info('Wrote to file %r', output_file)
     return 0
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(levelname)-5s:  %(message)s',
+        level=os.getenv('BIOSCRIPT_LOG_LEVEL', default=DEFAULT_LOG_LEVEL))
     if len(sys.argv) == 4:
         sys.exit(main(*sys.argv[1:4]))
+    elif len(sys.argv) == 3:
+        sys.exit(main(*sys.argv[1:3], DEFAULT_PATH_MODIFIER))
     elif len(sys.argv) == 1:
         sys.exit(main(DEFAULT_GENUS, DEFAULT_INPUT_FILE, DEFAULT_PATH_MODIFIER))
     else:
