c899f0097dc12f51648ba23224c4221d387ccb20
Hal Canary <halcanary@gmail.com>
2023-03-02 10:58:52 -0500

    more refactoring, style changes

--- bestSequenceEachSpecies.py
+++ bestSequenceEachSpecies.py
@@ -34,10 +34,11 @@ import os
 import sys
 
 
-###############################################################################
+###################################################################################################
 # FASTA file format parsing functions
 # These may be moved to another package at a later time.
-###############################################################################
+###################################################################################################
+
 
 def parse_fasta_format(f):
     '''
@@ -77,48 +78,29 @@ def split_str(s, n):
     return '\n'.join(s[i:i+n] for i in range(0, len(s), n))
 
 
-###############################################################################
+###################################################################################################
 
-def get_values_matching_genus(given_genus, fasta_source, logger):
-    '''
-    @param given_genus If not '', will filter the sequences by genus.
-    @param fasta_source FASTA source, a list (or generator) of tuples of
-           form `(description, value)`.
-           Each description line is expected to be in the form:
-           >ACCESSION_NUMBER GENUS EPITHET EXTRA ...
-    @return tuple of (count, dictionary) where count is the number of read
-            sequences from the source and the dictionary is
-            keyed by species ("{GENUS}_{EPITHET}") with values
-            that are lists of tuples, where each tuple is
-            `(accession_number, changed_description, sequence)`.
-            The new decription is the form:
-            >GENUS_EPITHET ACCESSION_NUMBER EXTRA ...
-    @raises ValueError if description has less than three fields.
-    '''
-    count, all_values = 0, collections.defaultdict(list)
-    for (description, value) in fasta_source:
-        count += 1
-        try:
-            accession, genus, epithet, extra = description.split(maxsplit=3)
-        except ValueError:
-            # `split` returned less than four elements; try again, assuming
-            # that extra is empty.
-            try:
-                accession, genus, epithet = description.split(maxsplit=2)
-                extra = ''
-            except ValueError:
-                # `description` is missing an epithet; this is an error.
-                raise ValueError(
-                    'Unable to parse description: %r' % description)
-        if given_genus and genus != given_genus:
-            logger.debug('BAD MATCH:  %s', description)
-            continue
-        logger.debug('good match: %s', description)
 
-        species = '%s_%s' % (genus, epithet)
-        new_description = ' '.join([species, accession, extra])
-        all_values[species].append((accession, new_description, value))
-    return count, dict(all_values)
+ProcessedDescription = collections.namedtuple(
+    'ProcessedDescription', ['genus', 'species', 'accession', 'description'])
+
+
+def process_sequence_description(description):
+    '''
+    Expect description to be a string of the form:
+    "ACCESSION GENUS EPITHET [MORE...]"
+    New description is:
+    "GENUS_EPITHET ACCESSION [MORE...]"
+    '''
+    fields = description.split()
+    if len(fields) < 3:
+        # `description` is missing an epithet; this is an error.
+        raise ValueError('Unable to parse description: %r' % description)
+    (accession, genus, epithet), extra = fields[:3], ' '.join(fields[3:])
+
+    species = '%s_%s' % (genus, epithet)
+    new_description = ' '.join([species, accession, extra])
+    return ProcessedDescription(genus, species, accession, new_description)
 
 
 def get_score(accession, description, sequence):
@@ -134,7 +116,7 @@ def get_score(accession, description, sequence):
     )
 
 
-def process_values(values):
+def get_best_sequence(values):
     '''
     @param values nonempty list of (accession, description, sequence) tuples.
     @return the best (description, sequence) pair based on `get_score`
@@ -146,8 +128,7 @@ def process_values(values):
     for accession, description, sequence in values:
         score = get_score(accession, description, sequence)
         if score >= best_score:
-            best_description, best_sequence = description, sequence
-            best_score = score
+            best_description, best_sequence, best_score = description, sequence, score
     assert best_description is not None and best_sequence is not None
     return (best_description, best_sequence)
 
@@ -156,61 +137,74 @@ def process_values(values):
 def get_best_sequence_each_species(infile, outfile, genus, logger):
     if genus:
         logger.info('Filtering by Genus %r', genus)
-    count, all_values = get_values_matching_genus(
-        genus, parse_fasta_format(infile), logger)
 
-    if len(all_values) == 0:
-        raise RuntimeError('None of %d sequences match given genus %r.' % (
-            count, genus))
+    sourceCount, speciesSequenceListMap = 0, collections.defaultdict(list)
+    for (description, sequence) in parse_fasta_format(infile):
+        sourceCount += 1
+        info = process_sequence_description(description)
+
+        if genus and info.genus != genus:
+            logger.debug('BAD MATCH:  %s', description)
+            continue
+        logger.debug('good match: %s', description)
+
+        speciesSequenceListMap[info.species].append((info.accession, info.description, sequence))
+
+    if len(speciesSequenceListMap) == 0:
+        raise RuntimeError('None of %d sequences match given genus %r.' % (sourceCount, genus))
 
     if logger.isEnabledFor(logging.INFO):
-        matchcount = sum(len(v) for v in all_values.values())
-        logger.info('Matched %d of %d sequences.', matchcount, count)
+        matchCount = sum(len(v) for v in speciesSequenceListMap.values())
+        logger.info('Matched %d of %d sequences.', matchCount, sourceCount)
 
     # Sort output by sepcies for reproducability.
-    for species, values in sorted(all_values.items()):
+    for species, values in sorted(speciesSequenceListMap.items()):
         try:
-            best_description, best_sequence = process_values(values)
+            best_description, best_sequence = get_best_sequence(values)
         except Exception as e:
-            raise RuntimeError(
-                "Error with species %s" % species) from e
+            raise RuntimeError("Error with species %s" % species) from e
         print_fasta_description(outfile, best_description, best_sequence)
         logger.debug('Best of %3d for species %r', len(values), species)
 
-    logger.info('%d species processed.', len(all_values))
+    logger.info('%d species processed.', len(speciesSequenceListMap))
 
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(
-        exit_on_error=False,
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=__doc__)
+        exit_on_error=False, description=__doc__)
     parser.add_argument(
-        'INFILE', nargs='?', type=argparse.FileType('r'),
+        'INFILE',
+        nargs='?',
+        type=argparse.FileType('r'),
         default=sys.stdin,
         help='Path of FASTA file to read. (default: STDIN)')
     parser.add_argument(
-        '-o', '--outfile', nargs='?',
-        type=argparse.FileType('w'), default=sys.stdout,
+        '-o',
+        '--outfile',
+        nargs='?',
+        type=argparse.FileType('w'),
+        default=sys.stdout,
         help='Where to write FASTA file. (default: STDOUT)')
     parser.add_argument(
-        '-g', '--genus', help='Genus to filter on. (default: None)')
+        '-g',
+        '--genus',
+        help='Genus to filter on. (default: None)')
     parser.add_argument(
-        '--loglevel', choices=['debug', 'info', 'warning'],
-        default='info', help='Verbosity level. (default: info)')
+        '--loglevel',
+        choices=['debug', 'info', 'warning'],
+        default='info',
+        help='Verbosity level. (default: info)')
     return parser.parse_args(argv)
 
-###############################################################################
+###################################################################################################
 
 
 def main():
     args = parse_args(sys.argv[1:])
-    logging.basicConfig(
-        format='%(levelname)s:  %(message)s',
-        level=args.loglevel.upper())
+    logging.basicConfig(format='%(levelname)s:  %(message)s', level=args.loglevel.upper())
     try:
-        get_best_sequence_each_species(
-            args.INFILE, args.outfile, args.genus, logging.getLogger())
+        get_best_sequence_each_species(args.INFILE, args.outfile, args.genus, logging.getLogger())
     except Exception as e:
         logging.error(e)
         sys.exit(1)
--- test_bioscript.py
+++ test_bioscript.py
@@ -11,10 +11,11 @@ import unittest
 
 import bestSequenceEachSpecies as bioscript
 
+
 TestSequence = collections.namedtuple(
     'TestSequence', [
-        'accession', 'genus', 'epithet', 'extra',
-        'description', 'sequence', 'fasta', 'species', 'translated'])
+        'accession', 'genus', 'epithet', 'extra', 'sequence',
+        'description', 'fasta', 'species', 'translated'])
 
 
 def makeTestSequence(accession, genus, epithet, extra, sequence):
@@ -22,31 +23,40 @@ def makeTestSequence(accession, genus, epithet, extra, sequence):
     fasta = '>%s\n%s\n\n' % (description, bioscript.split_str(sequence, 70))
     species = genus + '_' + epithet
     translated = ' '.join([species, accession, extra])
-    return TestSequence(accession, genus, epithet, extra, description,
-                        sequence, fasta, species, translated)
+    return TestSequence(accession, genus, epithet, extra, sequence,
+                        description, fasta, species, translated)
 
 
 def makeRandomSequence(seed):
     r = random.Random(seed)
     n = r.randrange(700, 2100)
-    weights = [99, 99, 99, 99, 1, 1, 1, 1, 1]
-    return ''.join(r.choices('ACGTYKRSW', weights=weights, k=n))
+    return ''.join(r.choices('ACGTYKRSW', weights=[99, 99, 99, 99, 1, 1, 1, 1, 1], k=n))
+
+
+def fasta_string(sequences):
+    b = io.StringIO()
+    for (description, sequence) in sequences:
+        bioscript.print_fasta_description(b, description, sequence)
+    return b.getvalue()
+
+
+def run_test_get_best_sequence_each_species(data, genus):
+    buffer = io.StringIO()
+    bioscript.get_best_sequence_each_species(io.StringIO(data), buffer, genus, logging.getLogger())
+    return buffer.getvalue()
 
 
 TESTDATA_1 = makeTestSequence(
     'KF787109.1', 'Arthrobacter', 'nicotianae',
-    'strain BSc 4 16S ribosomal RNA gene, partial sequence',
-    makeRandomSequence(1))
+    'strain BSc 4 16S ribosomal RNA gene, partial sequence', makeRandomSequence(1))
 
 TESTDATA_2 = makeTestSequence(
     'KP684097.1', 'Arthrobacter', 'nicotianae',
-    'strain BS20 16S ribosomal RNA gene, partial sequence',
-    makeRandomSequence(2))
+    'strain BS20 16S ribosomal RNA gene, partial sequence', makeRandomSequence(2))
 
 TESTDATA_3 = makeTestSequence(
     'NR_947583.1', 'Glutamicibacter', 'arilaitensis',
-    'strain Ind Int 3 16S ribosomal RNA gene, type strain Foo',
-    makeRandomSequence(3))
+    'strain Ind Int 3 16S ribosomal RNA gene, type strain Foo', makeRandomSequence(3))
 
 
 class NewDatabaseOneSequencePerSpeciesTestCase(unittest.TestCase):
@@ -65,77 +75,68 @@ class NewDatabaseOneSequencePerSpeciesTestCase(unittest.TestCase):
     def test_parse_fasta_format_2(self):
         t1, t2 = TESTDATA_1, TESTDATA_2
         self.assertEqual(
-            list(bioscript.parse_fasta_format(
-                    io.StringIO(t1.fasta + t2.fasta))), [
+            list(bioscript.parse_fasta_format(io.StringIO(t1.fasta + t2.fasta))), [
                 (t1.description, t1.sequence),
                 (t2.description, t2.sequence)])
 
     def test_print_fasta_description_1(self):
         self.maxDiff = None
-        t1, t2 = TESTDATA_1, TESTDATA_2
-        buffer = io.StringIO()
-        bioscript.print_fasta_description(buffer, t1.description, t1.sequence)
-        bioscript.print_fasta_description(buffer, t2.description, t2.sequence)
-        self.assertEqual(buffer.getvalue(), t1.fasta + t2.fasta)
-
-    def test_get_values_matching_genus_1(self):
-        t1, t2, t3 = TESTDATA_1, TESTDATA_2, TESTDATA_3
+        td = [TESTDATA_1, TESTDATA_2, TESTDATA_3]
         self.assertEqual(
-            bioscript.get_values_matching_genus('', [
-                (t1.description, t1.sequence),
-                (t2.description, t2.sequence),
-                (t3.description, t3.sequence)], logging.getLogger()),
-            (3, {t1.species: [(t1.accession, t1.translated, t1.sequence),
-                              (t2.accession, t2.translated, t2.sequence)],
-                 t3.species: [(t3.accession, t3.translated, t3.sequence)]}))
+            fasta_string([(t.description, t.sequence) for t in td]), ''.join(t.fasta for t in td))
 
-    def test_get_values_matching_genus_2(self):
-        self.assertEqual(
-            bioscript.get_values_matching_genus(
-                TESTDATA_3.species,
-                [(TESTDATA_1.description, TESTDATA_1.sequence)],
-                logging.getLogger()),
-            (1, {}))
+    def test_process_sequence_description(self):
+        for t in [TESTDATA_1, TESTDATA_2, TESTDATA_3]:
+            self.assertEqual(
+                bioscript.process_sequence_description(t.description),
+                (t.genus, t.species, t.accession, t.translated))
 
-    def test_get_values_matching_genus_3(self):
-        t1, t2, t3 = TESTDATA_1, TESTDATA_2, TESTDATA_3
+    def test_get_best_sequence_each_species_1(self):
+        example = ''.join(t.fasta for t in [TESTDATA_1, TESTDATA_2, TESTDATA_3])
         self.assertEqual(
-            bioscript.get_values_matching_genus(t1.genus, [
-                (t1.description, t1.sequence),
-                (t2.description, t2.sequence),
-                (t3.description, t3.sequence)], logging.getLogger()),
-            (3, {t1.species: [(t1.accession, t1.translated, t1.sequence),
-                              (t2.accession, t2.translated, t2.sequence)]}))
+            run_test_get_best_sequence_each_species(example, None),
+            fasta_string([(t.translated, t.sequence) for t in [TESTDATA_2, TESTDATA_3]]))
+        self.assertEqual(
+            run_test_get_best_sequence_each_species(example, TESTDATA_1.genus),
+            fasta_string([(TESTDATA_2.translated, TESTDATA_2.sequence)]))
+        self.assertEqual(
+            run_test_get_best_sequence_each_species(example, TESTDATA_3.genus),
+            fasta_string([(TESTDATA_3.translated, TESTDATA_3.sequence)]))
 
-    def test_process_values_1(self):
-        t1 = TESTDATA_1
-        self.assertEqual(
-            bioscript.process_values([
-                (t1.accession, t1.translated, t1.sequence)]),
-            (t1.translated, t1.sequence))
+    def test_get_best_sequence_each_species_2(self):
+        example = ''.join(t.fasta for t in [TESTDATA_1, TESTDATA_2, TESTDATA_3])
+        foundException = False
+        try:
+            run_test_get_best_sequence_each_species(example, 'Foobar')
+        except RuntimeError as e:
+            foundException = True
+        self.assertTrue(foundException)
 
-    def test_process_values_2(self):
-        t1, t2 = TESTDATA_1, TESTDATA_2
+    def test_get_best_sequence_1(self):
+        td = [(t.accession, t.translated, t.sequence) for t in [TESTDATA_1]]
         self.assertEqual(
-            bioscript.process_values([
-                (t1.accession, t1.translated, t1.sequence),
-                (t2.accession, t2.translated, t2.sequence)]),
-            (t2.translated, t2.sequence))
+            bioscript.get_best_sequence(td),
+            (TESTDATA_1.translated, TESTDATA_1.sequence))
+
+    def test_get_best_sequence_2(self):
+        td = [(t.accession, t.translated, t.sequence) for t in [TESTDATA_1, TESTDATA_2]]
+        self.assertEqual(
+            bioscript.get_best_sequence(td),
+            (TESTDATA_2.translated, TESTDATA_2.sequence))
 
     def test_get_score(self):
-        s = [
-            (0, 962.0/975,   0, 975),
-            (0, 805.0/815,   0, 815),
-            (1, 1173.0/1187, 1, 1187),
+        v = [
+            (TESTDATA_1, (0, 962.0/975,   0, 975)),
+            (TESTDATA_2, (0, 805.0/815,   0, 815)),
+            (TESTDATA_3, (1, 1173.0/1187, 1, 1187)),
         ]
-        td = [TESTDATA_1, TESTDATA_2, TESTDATA_3]
-        self.assertEqual(s, [bioscript.get_score(
-            t.accession, t.description, t.sequence) for t in td])
-        self.assertTrue(s[1] > s[0])
-        self.assertTrue(s[2] > s[0])
-        self.assertTrue(s[2] > s[1])
+        for t, s in v:
+            self.assertEqual(s, bioscript.get_score(t.accession, t.description, t.sequence))
+        self.assertTrue(v[1][1] > v[0][1])
+        self.assertTrue(v[2][1] > v[0][1])
+        self.assertTrue(v[2][1] > v[1][1])
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(levelname)s:  %(message)s', level='INFO')
+    logging.basicConfig(format='%(levelname)s:  %(message)s', level='WARNING')
     unittest.main()
